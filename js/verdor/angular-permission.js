8185d834340a38bc9957653448b2c5c8cf7533e5
reallax <345186403@qq.com>
2016-03-26 19:08:40 +0800

    add login page

--- js/verdor/angular-permission.js
+++ js/verdor/angular-permission.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module("ngPermission", [])
-.run(["$rootScope", function($rootScope) {
+.run(["$rootScope", "$location", "$window",  function($rootScope, $location, $window, $cookieStore) {
 	$rootScope.$on("$stateChangeStart", 
 	function(evt, toState, toParams, fromState, fromParams, Options){
 		console.info(evt);
@@ -10,5 +10,10 @@ angular.module("ngPermission", [])
 		console.info(fromState);
 		console.info(fromParams);
 		console.info(Options);
+		
+		console.info($location.path());
+//		console.info($cookieStore);
+//		$window.open("www.baidu.com");
+//		$window.location.href = "login/login.html";
 	});
 }]);
